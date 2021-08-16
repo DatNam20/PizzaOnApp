8c4535241ea0946fe2a6545fdb349ae2f010e43f
DEEPALI SINGH <deepalisingh15899@gmail.com>
2021-08-17 01:08:38 +0530

    Added layout for user's order

---
+++ app/src/main/java/com/example/pizzaon/UsersOrderActivity.java
@@ -0,0 +1,14 @@
+package com.example.pizzaon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UsersOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users_order);
+    }
+}
