39d6387e2daf894e0ed72c01446f2269fb5bbda9
DEEPALI SINGH <deepalisingh15899@gmail.com>
2021-08-19 20:36:19 +0530

    added item detail layout

---
+++ app/src/main/java/com/example/pizzaon/ItemDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.pizzaon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class ItemDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_detail);
+    }
+}
